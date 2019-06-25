3841d47a2ae478d4f74cd1a441a8d583fd3b04c9
Nate <nathanperfetti@gmail.com>
2019-06-24 19:01:47 -0500

    Set interval from per min to per 10mins

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var expresshbs = require("express-handlebars");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 var axios = require("axios");
+var cheerio = require("cheerio");
 
 // Set up port and mongodb connection
 var PORT = process.env.PORT || 3000;
@@ -10,17 +11,40 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/articleScraper
 
 const cron = require('node-cron');
 
-var task = cron.schedule(' * * * * *', () => {
+var task = cron.schedule(' */10 * * * *', () => {
 
   var dateTimeStamp = new Date().toLocaleString();
-  console.log('---Printing current date-time ('+dateTimeStamp+') every minute in the terminal---');
-  
-  // axios.get("https://www.youtube.com/trending").then(function (ytResponse) {
+  console.log('---Printing current date-time (' + dateTimeStamp + ') every minute in the terminal---');
 
-  // console.log("----------Youtube Scrape---------------");
-  
-  // console.log(ytResponse);
-  // });
+  axios.get("https://www.youtube.com/trending").then(function (ytResponse) {
+    var links = {};
+    var index = 0;
+
+    var $ = cheerio.load(ytResponse.data);
+
+    $("a").each(function (i, element) {
+  console.log("Inside each loop============");
+      //console.log("I: ",i," ELEMENT: ",element);
+
+      console.log("---------Parent Node: ",$(this).parent().attr("id"));
+      let title = $(this)
+      .text();
+      let thumbLink = $(this)
+      //.children("img")
+      //.attr("src");
+
+
+      //link = ($(this).attr('href'));
+      //links["link-"+index] = link;
+      index = index +1;
+
+
+    console.log("--This index:" +index);
+    console.log("--This Link:" +thumbLink);
+  });
+  console.log("----------Youtube Scrape---------------\n"+JSON.stringify(links));
+  });
+
 
 
 });
@@ -32,11 +56,15 @@ var app = express();
 // Use morgan logger for logging requests
 app.use(logger("dev"));
 // Parse request body as JSON
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
-app.engine("handlebars", expresshbs({ defaultLayout: "main" }));
+app.engine("handlebars", expresshbs({
+  defaultLayout: "main"
+}));
 app.set("view engine", "handlebars");
 
 // Routes/Controllers
@@ -44,7 +72,10 @@ var routes = require("./controllers/article_controller.js");
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useFindAndModify: false });
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
 // Start the server
 app.listen(PORT, function () {
